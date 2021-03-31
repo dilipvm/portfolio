31a06d394f608ede97831683545e47b668c5bb67
dilipvm <80629040+dilipvm@users.noreply.github.com>
2021-03-31 13:25:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+/*
+function dosomething(){
+    alert("My Name is dilip");
+}*/
