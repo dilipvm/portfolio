7ab430ca86d45dd3e7ce3c8e68cbeeffbca1aaec
dilipvm <80629040+dilipvm@users.noreply.github.com>
2022-11-06 13:45:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// toggle menu/navbar script
-$('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
-});
-/*
-function dosomething(){
-    alert("My Name is dilip");
-}*/
